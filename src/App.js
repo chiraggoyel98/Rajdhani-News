a9ef2a35fa80e5a670a59c1af2331e338575b03e
Chirag <chiraggoyel98@gmail.com>
2022-08-06 10:55:50 +0530

    Added news item comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './myComponents/Navbar';
+import News from './myComponents/News';
 
 export default class App extends Component {
+
   render() {
     return (
-      <div>App</div>
+      <div>
+      <Navbar />
+      <News />
+      </div>
     )
   }
 }
---
+++ src/myComponents/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+   
+  render() {
+    let {title,description,imageUrl,newsUrl} = this.props
+
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default NewsItem
